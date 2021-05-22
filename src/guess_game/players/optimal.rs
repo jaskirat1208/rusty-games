4c16f5c41ba0fbf829dbd0312ad132e7353392ef
jaskirat1208 <jaskirat1208@users.noreply.github.com>
2021-05-23 02:02:24 +0530

    Jaskirat (#4)
    * Removed lint errors
    
    * Converted the package to library. Added usage examples
    
    * Modified travis ci
    
    * Modified travis ci
    
    * Added clippy check to travis ci
    
    * Added clippy check to travis ci
    
    * Added lint checks
    
    * Fixed ci issue
    
    * Removed unnecessary commands of doc uploader
    
    * Renamed module to puzzle_games

--- src/guess_game/players/optimal.rs
+++ src/guess_game/players/optimal.rs
@@ -1,6 +1,12 @@
 use crate::guess_game;
+use std::cmp;
 use std::cmp::Ordering;
 
+/**
+ * Whenever the computer receives info about the turn played by a player,
+ * it will try to update the range of the target number. The next move
+ * it plays is the mean of this range.
+ */
 pub struct ComputerHard {
     m_name: String,
     _m_id: i32,
@@ -9,6 +15,9 @@ pub struct ComputerHard {
 }
 
 impl ComputerHard {
+    /**
+     * Returns a new computer hard object
+     */
     pub fn new(id: i32, name: String) -> ComputerHard {
         ComputerHard {
             m_name: name,
@@ -20,23 +29,38 @@ impl ComputerHard {
 }
 
 impl ComputerHard {
+    /**
+     * Returns the name of the bot.
+     */
     pub fn name(&self) -> String {
         self.m_name.to_string()
     }
 }
 
 impl guess_game::Play for ComputerHard {
+    /**
+     * Plays the next move. Returns the middle value of the range
+     * Called on turn.
+     */
     fn play(&self) -> String {
         (self.m_start + (self.m_end - self.m_start) / 2).to_string()
     }
 }
 
 impl guess_game::UpdateGameState for ComputerHard {
+    /**
+     * Updates the state of the game.
+     * Called after a player plays its move
+     */
     fn update_game_state(&mut self, turn: &guess_game::Turn) {
         match turn {
             guess_game::Turn::GuessingGame(turn) => match turn.board_response.result {
-                Ordering::Less => self.m_start = turn.board_response.move_played as i32,
-                Ordering::Greater => self.m_end = turn.board_response.move_played as i32,
+                Ordering::Less => {
+                    self.m_start = cmp::max(self.m_start, turn.board_response.move_played) as i32
+                }
+                Ordering::Greater => {
+                    self.m_end = cmp::min(self.m_end, turn.board_response.move_played) as i32
+                }
                 _ => {
                     self.m_start = turn.id as i32;
                     self.m_end = turn.id as i32;
